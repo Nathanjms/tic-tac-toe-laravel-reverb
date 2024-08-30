f57cb84330dc946016ea9cebfbdf9d020bd5034e
Nathan James <nathanjms@outlook.com>
2024-08-30 15:56:45 +0100

    Handle win/loss/draw

--- resources/js/Pages/Games/Show.vue
+++ resources/js/Pages/Games/Show.vue
@@ -3,14 +3,51 @@ import { ref } from "vue";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import { computed } from "vue";
 
+// Crosses are -1, noughts are 1
 const boardState = ref([0, 0, 0, 0, 0, 0, 0, 0, 0]);
 
 const isXTurn = computed(
     () => boardState.value.reduce((carry, value) => carry + value, 0) === 0
 );
 
+const lines = [
+    // rows
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    // columns
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    // diagonals
+    [0, 4, 8],
+    [2, 4, 6],
+];
+
 const fillSquare = (index) => {
     boardState.value[index] = isXTurn.value ? -1 : 1;
+
+    // Check for a win;
+    const winningLine = lines
+        .map((line) =>
+            line.reduce((carry, index) => carry + boardState.value[index], 0)
+        )
+        .find((sum) => Math.abs(sum) === 3);
+
+    if (winningLine === -3) {
+        alert("X wins!");
+        return;
+    }
+
+    if (winningLine === 3) {
+        alert("O wins!");
+        return;
+    }
+
+    if (boardState.value.every((square) => square !== 0)) {
+        alert("Draw!");
+        return;
+    }
 };
 </script>
 
