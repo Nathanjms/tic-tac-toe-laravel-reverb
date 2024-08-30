2e0061520aad5412b35b5a7ef409e062ee402b9c
Nathan James <nathanjms@outlook.com>
2024-08-30 15:50:05 +0100

    Board basics

--- resources/js/Pages/Games/Show.vue
+++ resources/js/Pages/Games/Show.vue
@@ -1,10 +1,34 @@
 <script setup>
+import { ref } from "vue";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import { computed } from "vue";
+
+const boardState = ref([0, 0, 0, 0, 0, 0, 0, 0, 0]);
+
+const isXTurn = computed(
+    () => boardState.value.reduce((carry, value) => carry + value, 0) === 0
+);
+
+const fillSquare = (index) => {
+    boardState.value[index] = isXTurn.value ? -1 : 1;
+};
 </script>
 
 <template>
     <AuthenticatedLayout>
-        Testing!
-        {{ $page.props }}
+        <menu class="grid grid-cols-3 gap-1.5 w-0 min-w-fit mx-auto mt-12">
+            <li
+                v-for="(square, index) in boardState"
+                :key="square"
+                class="bg-gray-300 size-32 grid place-items-center text-4xl"
+            >
+                <button
+                    v-if="square === 0"
+                    @click="() => fillSquare(index)"
+                    class="place-self-stretch bg-gray-200 hover:bg-gray-300 transition-colors"
+                ></button>
+                <span v-else>{{ square === -1 ? "X" : "O" }}</span>
+            </li>
+        </menu>
     </AuthenticatedLayout>
 </template>
